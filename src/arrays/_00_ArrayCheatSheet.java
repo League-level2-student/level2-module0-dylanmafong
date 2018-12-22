8c3ab7e95aed328195075f4ea90abe5625e82163
LucasKhattar <League@AdministorsiMac.attlocal.net>
2018-12-21 16:59:52 -0800

    Level 2array

--- src/arrays/_00_ArrayCheatSheet.java
+++ src/arrays/_00_ArrayCheatSheet.java
@@ -3,25 +3,31 @@ package arrays;
 public class _00_ArrayCheatSheet {
 	public static void main(String[] args) {
 		//1. make an array of 5 Strings
-		
-		//2. print the third element in the array
-		
+		String [] x = new String[5];
+ 		//2. print the third element in the array
+		System.out.println(x[2]);
 		//3. set the third element to a different value
-		
+		x[2] = "hi";
 		//4. print the third element again
-		
+		System.out.println(x[2]);
 		//5. use a for loop to print all the values in the array
 		//   BE SURE TO USE THE ARRAY'S length VARIABLE
-		
+		for (int i = 0; i < x.length; i++) {
+			
+		}
 		
 		//6. make an array of 50 integers
-		
+		    String [] x = new String [5];
 		//7. use a for loop to make every value of the integer array a random number
-		
+		for (int i = 50; i < x.length; i++) {
+			
+		}
 		//8. without printing the entire array, print only the smallest number in the array
 		
 		//9 print the entire array to see if step 8 was correct
-		
+		System.out.println();
 		//10. print the largest number in the array.
+		System.out.println();
 	}
+	
 }
