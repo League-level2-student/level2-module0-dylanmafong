21481c84d53fcecdd9c24648d127ed6087503e74
dylanmafong <League@administorsimac.attlocal.net>
2019-01-26 11:30:38 -0800

    arrays

--- src/arrays/_00_ArrayCheatSheet.java
+++ src/arrays/_00_ArrayCheatSheet.java
@@ -1,5 +1,7 @@
 package arrays;
 
+import java.util.Random;
+
 public class _00_ArrayCheatSheet {
 	public static void main(String[] args) {
 		//1. make an array of 5 Strings
@@ -13,21 +15,38 @@ public class _00_ArrayCheatSheet {
 		//5. use a for loop to print all the values in the array
 		//   BE SURE TO USE THE ARRAY'S length VARIABLE
 		for (int i = 0; i < x.length; i++) {
-			
+			System.out.println(x[i]);
 		}
 		
 		//6. make an array of 50 integers
-		    String [] x = new String [5];
+		    int [] y = new int[50];
 		//7. use a for loop to make every value of the integer array a random number
-		for (int i = 50; i < x.length; i++) {
-			
+		for (int i = 0; i < y.length; i++) {
+			y[i] = new Random().nextInt();
 		}
 		//8. without printing the entire array, print only the smallest number in the array
+		int small = y[0];
+		for (int i = 0; i < y.length; i++) {
+			if(small>y[i]) {
+				small = y[i];
+			}
+		}
+		System.out.println("Smallest: "+small);
 		
 		//9 print the entire array to see if step 8 was correct
-		System.out.println();
-		//10. print the largest number in the array.
-		System.out.println();
+		 for (int i = 0; i < 50; i++) {
+				System.out.println(y[i]);
+		}
+
+		
+	//10. print the largest number in the array.
+		 int big = y[0];
+		for (int i = 0; i < y.length; i++) {
+			if(big < y[i]) {
+				big = y[i];
+			}
+		}
+		System.out.println("Biggest: "+big);
 	}
 	
 }
--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -1,12 +1,18 @@
 package arrays;
 
+import java.awt.Robot;
+
 public class _01_RobotRace {
 	//1. make a main method
+public static void main(String[] args) {
+	
 
 	//2. create an array of 5 robots.
-
+	Robot [] x = new Robot[5];
 	//3. use a for loop to initialize the robots.
-
+for (int i = 0; i < x.length; i++) {
+	
+}
 		//4. make each robot start at the bottom of the screen, side by side, facing up
 
 	//5. use another for loop to iterate through the array and make each robot move 
@@ -19,4 +25,5 @@ public class _01_RobotRace {
 	//8. try different races with different amounts of robots.
 
 	//9. make the robots race around a circular track.
+}
 }
