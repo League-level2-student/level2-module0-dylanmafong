e46e236faec37743047064fe093deada696a6d8b
zzmimicoder <League@AdministorsiMac.attlocal.net>
2019-02-20 16:57:56 -0800

    awesomeness
    sdfasdf

--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -2,11 +2,16 @@ package arrays;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class _01_RobotRace {
 	//1. make a main method
+		
 public static void main(String[] args) {
+	Random a = new Random();
+
 	
 
 	//2. create an array of 5 robots.
@@ -16,24 +21,28 @@ for (int i = 0; i < rob.length; i++) {
 	 rob[i]= new Robot();
 
 		//4. make each robot start at the bottom of the screen, side by side, facing up
-		rob[i].setX(140+i*150);
+		rob[i].setX(140+i*100);
 		rob[i].setY(500);
 }//5. use another for loop to iterate through the array and make each robot move 
 	//   a random amount less than 50.
 for (int i = 0; i < rob.length; i++) {
-	Random a = new Random(50);
+int num = a.nextInt(50);
+rob[i].move(num);
 }
 
 	//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
-	for (int i = 0; i < rob.length; i++) {
-		
-	}
-	//7. declare that robot the winner and throw it a party!
 
+	
+}
+	//7. declare that robot the winner and throw it a party!
+	 {
+	
+JOptionPane.showMessageDialog(null, "");
+	}
 	//8. try different races with different amounts of robots.
 
 	//9. make the robots race around a circular track.
 }
-}
+
 
 
