9ba6fe0526b39208205ae3b07dccd10f5d3ae6ae
dylanmafong <league@wts21.attlocal.net>
2019-03-01 16:57:42 -0800

    Robot game race

--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -7,42 +7,49 @@ import javax.swing.JOptionPane;
 import org.jointheleague.graphical.robot.Robot;
 
 public class _01_RobotRace {
-	//1. make a main method
-		
-public static void main(String[] args) {
-	Random a = new Random();
+	// 1. make a main method
 
-	
+	public static void main(String[] args) {
+		Random a = new Random();
 
-	//2. create an array of 5 robots.
- Robot[] rob = new Robot[5];
-	//3. use a for loop to initialize the robots.
-for (int i = 0; i < rob.length; i++) {
-	 rob[i]= new Robot();
+		// 2. create an array of 5 robots.
+		Robot[] rob = new Robot[5];
 
-		//4. make each robot start at the bottom of the screen, side by side, facing up
-		rob[i].setX(140+i*100);
-		rob[i].setY(500);
-}//5. use another for loop to iterate through the array and make each robot move 
-	//   a random amount less than 50.
-for (int i = 0; i < rob.length; i++) {
-int num = a.nextInt(50);
-rob[i].move(num);
-}
+		// 3. use a for loop to initialize the robots.
+		for (int i = 0; i < rob.length; i++) {
+			rob[i] = new Robot();
 
-	//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
+			// 4. make each robot start at the bottom of the screen, side by side, facing up
+			rob[i].setX(140 + i * 100);
+			rob[i].setY(500);
+		} // 5. use another for loop to iterate through the array and make each robot move
+			// a random amount less than 50.
+		for (int i = 0; i < rob.length; i++) {
+			int num = a.nextInt(100);
+			rob[i].setSpeed(10);
+			rob[i].move(num);
 
-	
-}
-	//7. declare that robot the winner and throw it a party!
-	 {
-	
-JOptionPane.showMessageDialog(null, "");
+		}
+		boolean isRaiseing = true;
+		// 6. use a while loop to repeat step 5 until a robot has reached the top of the
+		// screen.
+		while (isRaiseing) {
+			for (int i = 0; i < rob.length; i++) {
+				int num = a.nextInt(50)
+				rob[i].move(num);
+				if (rob[i].getY() <= 1) {
+				 isRaiseing = false;
+				 JOptionPane.showMessageDialog(null, "    Robot  " + i +  " winner!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+			}}
+		}
 	}
-	//8. try different races with different amounts of robots.
 
-	//9. make the robots race around a circular track.
+	// 7. declare that robot the winner and throw it a party!
+	{
+		
+	}
+	// 8. try different races with different amounts of robots.
+
+	// 9. make the robots race around a circular track.
+	
 }
-
-
-
